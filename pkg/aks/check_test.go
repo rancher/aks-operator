5cbb751943af59ef9604ef1c1c1a69b93eb3ebc9
Alexandr Demicev <alexandr.demicev@suse.com>
2022-12-09 17:55:03 +0100

    Switch to new services

--- pkg/aks/check_test.go
+++ pkg/aks/check_test.go
@@ -7,20 +7,20 @@ import (
 )
 
 func Test_generateUniqueLogWorkspace(t *testing.T) {
-  tests := []struct {
-    name          string
-    workspaceName string
-    want          string
-  }{
-    {
-      name:          "basic test",
-      workspaceName: "ThisIsAValidInputklasjdfkljasjgireqahtawjsfklakjghrehtuirqhjfhwjkdfhjkawhfdjkhafvjkahg",
-      want:          "ThisIsAValidInputklasjdfkljasjgireqahtawjsfkla-fb8fb22278d8eb98",
-    },
-  }
-  for _, tt := range tests {
-    got := generateUniqueLogWorkspace(tt.workspaceName)
-    assert.Equal(t, tt.want, got)
-    assert.Len(t, got, 63)
-  }
+	tests := []struct {
+		name          string
+		workspaceName string
+		want          string
+	}{
+		{
+			name:          "basic test",
+			workspaceName: "ThisIsAValidInputklasjdfkljasjgireqahtawjsfklakjghrehtuirqhjfhwjkdfhjkawhfdjkhafvjkahg",
+			want:          "ThisIsAValidInputklasjdfkljasjgireqahtawjsfkla-fb8fb22278d8eb98",
+		},
+	}
+	for _, tt := range tests {
+		got := generateUniqueLogWorkspace(tt.workspaceName)
+		assert.Equal(t, tt.want, got)
+		assert.Len(t, got, 63)
+	}
 }
--- pkg/aks/client.go
+++ pkg/aks/client.go
@@ -5,9 +5,6 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/Azure/azure-sdk-for-go/services/containerservice/mgmt/2020-11-01/containerservice"
-	"github.com/Azure/azure-sdk-for-go/services/operationalinsights/mgmt/2020-08-01/operationalinsights"
-	"github.com/Azure/azure-sdk-for-go/services/resources/mgmt/2019-10-01/resources"
 	"github.com/Azure/go-autorest/autorest"
 	"github.com/Azure/go-autorest/autorest/adal"
 	"github.com/Azure/go-autorest/autorest/azure"
@@ -34,54 +31,6 @@ type Credentials struct {
 	TenantID       string
 	ClientID       string
 	ClientSecret   string
-}
-
-func NewResourceGroupClient(cred *Credentials) (*resources.GroupsClient, error) {
-	authorizer, err := NewClientAuthorizer(cred)
-	if err != nil {
-		return nil, err
-	}
-
-	client := resources.NewGroupsClientWithBaseURI(to.String(cred.BaseURL), cred.SubscriptionID)
-	client.Authorizer = authorizer
-
-	return &client, nil
-}
-
-func NewClusterClient(cred *Credentials) (*containerservice.ManagedClustersClient, error) {
-	authorizer, err := NewClientAuthorizer(cred)
-	if err != nil {
-		return nil, err
-	}
-
-	client := containerservice.NewManagedClustersClientWithBaseURI(to.String(cred.BaseURL), cred.SubscriptionID)
-	client.Authorizer = authorizer
-
-	return &client, nil
-}
-
-func NewAgentPoolClient(cred *Credentials) (*containerservice.AgentPoolsClient, error) {
-	authorizer, err := NewClientAuthorizer(cred)
-	if err != nil {
-		return nil, err
-	}
-
-	agentProfile := containerservice.NewAgentPoolsClientWithBaseURI(to.String(cred.BaseURL), cred.SubscriptionID)
-	agentProfile.Authorizer = authorizer
-
-	return &agentProfile, nil
-}
-
-func NewOperationInsightsWorkspaceClient(cred *Credentials) (*operationalinsights.WorkspacesClient, error) {
-	authorizer, err := NewClientAuthorizer(cred)
-	if err != nil {
-		return nil, err
-	}
-
-	client := operationalinsights.NewWorkspacesClientWithBaseURI(to.String(cred.BaseURL), cred.SubscriptionID)
-	client.Authorizer = authorizer
-
-	return &client, nil
 }
 
 func NewClientAuthorizer(cred *Credentials) (autorest.Authorizer, error) {
@@ -143,7 +92,7 @@ func GetSecrets(secretsCache wranglerv1.SecretCache, secretClient wranglerv1.Sec
 	cred.BaseURL = spec.BaseURL
 
 	if cred.TenantID == "" {
-		cred.TenantID, err = GetCachedTenantID(secretClient, cred.SubscriptionID, secret)
+		cred.TenantID, err = getCachedTenantID(secretClient, cred.SubscriptionID, secret)
 		if err != nil {
 			return nil, err
 		}
@@ -156,7 +105,7 @@ type secretClient interface {
 	Update(*v1.Secret) (*v1.Secret, error)
 }
 
-func GetCachedTenantID(secretClient secretClient, subscriptionID string, secret *v1.Secret) (string, error) {
+func getCachedTenantID(secretClient secretClient, subscriptionID string, secret *v1.Secret) (string, error) {
 	annotations := secret.GetAnnotations()
 	tenantAnno, timestamp := annotations[tenantIDAnnotation], annotations[tenantIDTimestampAnnotation]
 	if tenantAnno != "" && timestamp != "" {
